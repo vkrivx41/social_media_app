b4ecae8fad24b3575566599ac3664fee1a9e820a
Pasxal Le Grand <vkrivx41@gmail.com>
2025-10-29 22:05:58 +0200

    4. Create db models (all)

--- app_contents/models.py
+++ app_contents/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from app_users.models import User
+
+class Post(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts")
+    description = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="images/posts/")
+
+    def __str__(self) -> str:
+        return f"Post: {self.pk} - {self.user}"
+    
--- app_users/models.py
+++ app_users/models.py
@@ -1,16 +1,22 @@
-# from django.db import models
-# from django.contrib.auth.models import AbstractUser
-# from django.utils import timezone
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# class User(AbstractUser):
-#     gender = models.CharField(choices=["Male", "Female"])
-#     date_joined = models.DateField(auto_now=timezone.now)
-                                   
-# class Post(models.Model):
-#     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-#     description = models.CharField(max_length=300)
-#     image = models.ImageField(upload_to="images/posts")
-                              
-# class Profile(models.Model):
-#     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-#     image = models.ImageField(upload_to="images/profiles")
+class User(AbstractUser):
+    class Gender(models.TextChoices):
+        male = "male", "Male"
+        female = "female", "Female"
+
+    gender = models.CharField(max_length=10, choices=Gender.choices)
+    date_joined = models.DateField(auto_now=timezone.now)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="profile")
+    image = models.ImageField(upload_to="images/profiles/")
+
+    def __str__(self):
+        return f"Profile: {self.user.username}"
